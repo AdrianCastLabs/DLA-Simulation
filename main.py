ffe40eca177fd8064b4741ae3ee65581d42096fc
Adrian Castillo <adrian.cast@icloud.com>
2026-05-27 23:21:02 +0200

    added particle collision handling

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from math import ceil
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-gridSize = 25
+gridSize = 30
 grid = np.zeros((gridSize, gridSize))
 
 xPos = ceil(gridSize / 2)
@@ -22,23 +22,43 @@ def MoveParticle(direction):
     newY = yPos
 
     if direction == 1:
+        grid[newX, newY] = 0
         newX += 1
     if direction == 2:
+        grid[newX, newY] = 0
         newY -= 1
     if direction == 3:
+        grid[newX, newY] = 0
         newX -= 1
     if direction == 4:
+        grid[newX, newY] = 0
         newY += 1
 
-    # Check bounds
-    if 0 <= newX < gridSize and 0 <= newY < gridSize:
+    if not CheckCollisions(newX, newY):
+        grid[xPos, yPos] = 1
+        xPos = ceil(gridSize / 2)
+        yPos = ceil(gridSize / 2)
+
+    if CheckCollisions(newX, newY):
         grid[newX, newY] = 1
         xPos = newX
         yPos = newY
 
+def CheckCollisions(newX, newY):
+
+    if not (1 <= newX < gridSize - 1 and 1 <= newY < gridSize - 1):
+        return False
+
+    isNotColliding = (
+        grid[newX + 1, newY] == 0 and
+        grid[newX - 1, newY] == 0 and
+        grid[newX, newY + 1] == 0 and
+        grid[newX, newY - 1] == 0
+    )
+
+    return isNotColliding
+
 def update(frame):
-
-
     MoveParticle(np.random.randint(1, 5))
 
     image.set_array(grid)
