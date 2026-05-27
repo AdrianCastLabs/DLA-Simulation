cf59358112f825c24ec5fc60bc96c76f73e1314d
Adrian Castillo <adrian.cast@icloud.com>
2026-05-27 23:37:51 +0200

    Spawns a seed particle at the center

--- main.py
+++ main.py
@@ -3,16 +3,40 @@ from math import ceil
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+
 gridSize = 120
 grid = np.zeros((gridSize, gridSize))
 
-xPos = ceil(gridSize / 2)
-yPos = ceil(gridSize / 2)
+grid[gridSize // 2, gridSize // 2] = 1
 
-grid[xPos, yPos] = 1
+def SpawnParticle():
+    side = np.random.randint(0, 4)
+
+    if side == 0:  # top
+        return np.random.randint(0, gridSize), 0
+
+    if side == 1:  # bottom
+        return np.random.randint(0, gridSize), gridSize - 1
+
+    if side == 2:  # left
+        return 0, np.random.randint(0, gridSize)
+
+    if side == 3:  # right
+        return gridSize - 1, np.random.randint(0, gridSize)
+
+
+xPos, yPos = SpawnParticle()
 
 fig, ax = plt.subplots()
 image = ax.imshow(grid, cmap='gray', vmin=0, vmax=1)
+
+def TouchingCluster(x, y):
+    return(
+        grid[x + 1, y] == 1 or
+        grid[x - 1, y] == 1 or
+        grid[x, y + 1] == 1 or
+        grid[x, y - 1] == 1
+    )
 
 def MoveParticle(direction):
     global xPos
@@ -22,43 +46,30 @@ def MoveParticle(direction):
     newY = yPos
 
     if direction == 1:
-        grid[newX, newY] = 0
         newX += 1
     if direction == 2:
-        grid[newX, newY] = 0
         newY -= 1
     if direction == 3:
-        grid[newX, newY] = 0
         newX -= 1
     if direction == 4:
-        grid[newX, newY] = 0
         newY += 1
 
-    if not CheckCollisions(newX, newY):
-        grid[xPos, yPos] = 1
-        xPos = ceil(gridSize / 2)
-        yPos = ceil(gridSize / 2)
-
-    if CheckCollisions(newX, newY):
-        grid[newX, newY] = 1
-        xPos = newX
-        yPos = newY
-
-def CheckCollisions(newX, newY):
-
     if not (1 <= newX < gridSize - 1 and 1 <= newY < gridSize - 1):
-        return False
+        xPos, yPos = SpawnParticle()
+        return
 
-    isNotColliding = (
-        grid[newX + 1, newY] == 0 and
-        grid[newX - 1, newY] == 0 and
-        grid[newX, newY + 1] == 0 and
-        grid[newX, newY - 1] == 0
-    )
+    if TouchingCluster(newX, newY):
+        grid[newX, newY] = 1
+        xPos, yPos = SpawnParticle()
+        return
 
-    return isNotColliding
+    xPos = newX
+    yPos = newY
+
+
 
 def update(frame):
+
     for i in range(10000):
         MoveParticle(np.random.randint(1, 5))
 
@@ -66,5 +77,7 @@ def update(frame):
 
     return [image]
 
+
 animation = FuncAnimation(fig, update, interval=0)
+
 plt.show()
